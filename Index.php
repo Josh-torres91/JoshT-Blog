14fe8e09bb7cb61d36ed9cff248c9b5d1eeec4b2
Joshua Torres <mjolnir_mkv@yahoo.com>
2015-02-05 09:19:28 -0800

    New commit for the blog project.

--- Index.php
+++ Index.php
@@ -1,10 +1,17 @@
+<html>
+    <head>
+        <title>JayBlog</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="http://www.halopedia.org/images/0/05/Marathon_logo.jpg">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="css/custom-style.css">
+</html>
+
 <?php
 require_once(__DIR__ . "/view/header.php");
 require_once(__DIR__ . "/view/navigation.php");
 require_once(__DIR__ . "/controller/create-db.php");
 require_once(__DIR__ . "/view/footer.php");
 ?>; 
-
-<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
-<link type="text/css" rel="stylesheet" href="css/bootstrap.css">
-<link type="text/css" rel="stylesheet" href="css/custom-style.css">
--- Post.php
+++ Post.php
@@ -1,5 +1,15 @@
-<?php
+<html>
+    <head>
+        <title>JayBlog</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="http://www.halopedia.org/images/0/05/Marathon_logo.jpg">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="css/custom-style.css">
+</html>
 
+<?php
 require_once(__DIR__ . "/View/Header.php");
 require_once(__DIR__ . "/View/Form.php");
 require_once(__DIR__ . "/View/Footer.php");
