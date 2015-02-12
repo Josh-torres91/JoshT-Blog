c8b4e391d0e32dc51eac1344b6c6f9870e3fed83
Joshua Torres <mjolnir_mkv@yahoo.com>
2015-02-12 09:17:03 -0800

    Final commit for the week!!!

--- Index.php
+++ Index.php
@@ -14,4 +14,5 @@ require_once(__DIR__ . "/view/header.php");
 require_once(__DIR__ . "/view/navigation.php");
 require_once(__DIR__ . "/controller/create-db.php");
 require_once(__DIR__ . "/view/footer.php");
+require_once(__DIR__ . "/controller/Read--posts.php");
 ?>; 
